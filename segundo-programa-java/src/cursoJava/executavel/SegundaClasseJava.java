b5ec0b03d94175e444f5e0e3e7e30d199fb23726
Caua Santana <santanacaua155@gmail.com>
2022-09-13 15:28:08 -0300

    VARIAVEL TIPO DOUBLE

---
+++ segundo-programa-java/src/cursoJava/executavel/SegundaClasseJava.java
@@ -0,0 +1,17 @@
+package cursoJava.executavel;
+
+public class SegundaClasseJava {
+	//metodo main é um metodo auto executavel no java
+		public static void main(String[] args) {
+			
+			double nota1 = 90;
+			double nota2 = 70;
+			double nota3 = 50;
+			double nota4 = 88;
+	 
+			double mediaFinal = (nota1 + nota2 + nota3 + nota4) / 4;
+			//Calculando a media das notas
+		System.out.println("A media das notas é = " + mediaFinal);
+			
+		}
+}
